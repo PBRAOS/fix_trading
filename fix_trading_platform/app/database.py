6610406376c3b869b20dd05c7895df93afe182d2
Panagiotis Braimakis <pbraimakis@yahoo.gr>
2025-07-26 13:12:32 +0300

    Update database.py
    Retry logic

--- fix_trading_platform/app/database.py
+++ fix_trading_platform/app/database.py
@@ -1,7 +1,25 @@
+import time
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
 
-DATABASE_URL = "postgresql://postgres:password@db:5432/trading"
+DATABASE_URL = "postgresql://postgres:Fraoules12@localhost:5432/db"
 
-engine = create_engine(DATABASE_URL)
+# Retry logic for DB connection
+max_tries = 10
+for i in range(max_tries):
+
+    print('Trying: ', str(i))
+
+    try:
+        engine = create_engine(DATABASE_URL)
+        engine.connect()
+        print("Connected to PostgreSQL")
+        break
+    except OperationalError:
+        print(f"Database not ready, retrying ({i + 1}/{max_tries})...")
+        time.sleep(2) # Make a small pause.
+else:
+    raise Exception("Database connection failed after retries")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
