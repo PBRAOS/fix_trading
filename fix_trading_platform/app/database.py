02809acab4df4270d1ef45ec67d6489c2d3de428
Panagiotis Braimakis <pbraimakis@yahoo.gr>
2025-08-01 20:00:44 +0300

    Fixes in Dockerfi;e and docker compose. Removed fix_handler and added correct credentials from app call
    Signed-off-by: Panagiotis Braimakis <pbraimakis@yahoo.gr>

--- fix_trading_platform/app/database.py
+++ fix_trading_platform/app/database.py
@@ -5,27 +5,26 @@ from sqlalchemy.exc import OperationalError
 import getpass
 
 print(getpass.getuser())
-if getpass.getuser()=='pbraimakis':
+if getpass.getuser() == 'pbraimakis':
     # LOCAL MAC
-    DATABASE_URL = "postgresql://postgres:Fraoules12@localhost:5433/db"
+    DATABASE_URL = "mssql+pyodbc://MySQL:Fr@0ules123!@localhost:1433/db?driver=ODBC+Driver+17+for+SQL+Server"
 else:
     # DOCKER
-    DATABASE_URL = "postgresql://postgres:Fraoules12@db:5432/db"
+    DATABASE_URL = "mssql+pyodbc://MySQL:Fr@0ules123!@mssql:1433/db?driver=ODBC+Driver+17+for+SQL+Server"
 
 # Retry logic for DB connection
 max_tries = 10
 for i in range(max_tries):
-
     print('Trying: ', str(i))
-
     try:
         engine = create_engine(DATABASE_URL)
         engine.connect()
-        print("Connected to PostgreSQL")
+        print("Connected to SQL Server")
         break
-    except OperationalError:
+    except OperationalError as e:
         print(f"Database not ready, retrying ({i + 1}/{max_tries})...")
-        time.sleep(2) # Make a small pause.
+        print(str(e))
+        time.sleep(2)
 else:
     raise Exception("Database connection failed after retries")
 
