0feec026b08ad08ee0ab1a7cfbd8aac266247521
Panagiotis Braimakis <pbraimakis@yahoo.gr>
2025-08-01 22:45:55 +0300

    Fixes: switched back to pytds for mac OS compatibility issues.
    Signed-off-by: Panagiotis Braimakis <pbraimakis@yahoo.gr>

--- fix_trading_platform/app/database.py
+++ fix_trading_platform/app/database.py
@@ -3,15 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import OperationalError
 import getpass
-import os
 
 print(getpass.getuser())
 if getpass.getuser() == 'pbraimakis':
     # LOCAL MAC
-    DATABASE_URL = DATABASE_URL = f"mssql+pyodbc://sa:Fr@0ules123@localhost:1433/master?driver=ODBC+Driver+17+for+SQL+Server"
+    DATABASE_URL = DATABASE_URL = f"mssql+pytds://sa:Fr@0ules123@localhost:1433/master"
 else:
     # DOCKER
-    DATABASE_URL = DATABASE_URL = f"mssql+pyodbc://sa:Fr@0ules123@db:1433/master?driver=ODBC+Driver+17+for+SQL+Server"
+    DATABASE_URL = DATABASE_URL = f"mssql+pytds://sa:Fr@0ules123@db:1433/master"
 
 # Retry logic for DB connection
 max_tries = 10
